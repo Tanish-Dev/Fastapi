428428fe55beede4d3921464edcd2bb7129b2c87
Tanish <tanishbhavsar2007@gmail.com>
2025-06-21 00:02:29 +0530

    addition of files

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/")
-def read_root():
-    return {"hello" : "World"}
-
-
+conn = MongoClient("mongodb+srv://Tanish-dev:xogtu2-kofwoj-sEpnef@cluster0.n4ytgbb.mongodb.net/")
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    docs = conn.notes.notes.findone({ })
+    for doc in docs:
+        print(doc)
+    return templates.TemplateResponse("index.html", {"request": request})
